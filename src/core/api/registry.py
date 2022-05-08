ce0e07649a6cae85234ddcedef54bab5208ddf4e
Carlos Pomares <cpomaresp@gmail.com>
2022-05-08 12:47:24 +0200

    Added server update method

--- src/core/api/registry.py
+++ src/core/api/registry.py
@@ -26,3 +26,23 @@ class RegistryAPI(APIComponent):
             if server.id == id:
                 return server.to_json()    
         raise Exception('Server not found')
+    
+    def registry_update(self, id: str):
+        self.__update_registry__()
+        registry_object = None
+        for server in self.registry.__get_registry_objects__():
+            if server.id == id:
+                registry_object = server
+        if registry_object is None:
+            raise Exception('Server not found')
+        # Update registry object
+        self.registry.__fetch_object__(
+            registry_object=registry_object,
+            configuration=self.api.configuration,
+            credentials=self.api.credentials,
+        )
+        self.__update_registry__()
+        for server in self.registry.__get_registry_objects__():
+            if server.id == id:
+                registry_object = server
+        return registry_object.to_json()
